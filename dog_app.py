59dadcde157369076075e42c9feadbf3a7cb41cf
ngrajles1 <15281436+ngrajales1@users.noreply.github.com>
2019-03-14 21:43:38 -0500

    Defined my first CNN architecture

--- dog_app.py
+++ dog_app.py
@@ -407,6 +407,14 @@ from keras.models import Sequential
 model = Sequential()
 
 ### TODO: Define your architecture.
+model.add(Conv2D(filters=16, kernel_size=2, padding='same', activation='relu', input_shape=(224,224,3)))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=32, kernel_size=2, padding='same',activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=64, kernel_size=2, padding='same',activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(GlobalAveragePooling2D())
+model.add(Dense(133, activation='softmax'))
 
 model.summary()
 
