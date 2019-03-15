36dacd6fad4dc8594979b6428b0d75a37e813d3f
ngrajles1 <15281436+ngrajales1@users.noreply.github.com>
2019-03-14 21:56:05 -0500

    Transfer learning, bottle neck features VGG19

--- dog_app.py
+++ dog_app.py
@@ -609,7 +609,10 @@ def VGG16_predict_breed(img_path):
 
 
 ### TODO: Obtain bottleneck features from another pre-trained CNN.
-
+bottleneck_features = np.load('/data/bottleneck_features/DogVGG19{Data.npz')
+train_VGG19 = bottleneck_features['train']
+valid_VGG19 = bottleneck_features['valid']
+test_VGG19 = bottleneck_features['test']
 
 # ### (IMPLEMENTATION) Model Architecture
 #
