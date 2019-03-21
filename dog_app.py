8e1f8b953ab5134df9f109e6e2283ee1c01ab6a6
ngrajles1 <15281436+ngrajales1@users.noreply.github.com>
2019-03-20 19:43:46 -0500

    VGG19 retrain = accuracy 48.44

--- dog_app.py
+++ dog_app.py
@@ -658,7 +658,7 @@ model.add(GlobalAveragePooling2D(input_shape=train_InceptionV3.shape[1:]))
 model.add(Dense(133, activation='softmax'))
 
 
-# ### (IMPLEMENTATION) Compile the Model
+# ### (IMPLEMENTATION) Compile the Model 
 
 # In[ ]:
 
